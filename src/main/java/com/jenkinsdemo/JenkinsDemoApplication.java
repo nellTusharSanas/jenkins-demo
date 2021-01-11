0d1eb73a57d27b8876992c2d4e8b98ca68c26558
tushar <tushar.sanas@nellinfotech.com>
2021-01-11 07:52:36 +0530

    third commit

--- src/main/java/com/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/jenkinsdemo/JenkinsDemoApplication.java
@@ -1,15 +1,12 @@
 package com.jenkinsdemo;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsDemoApplication {
-org.slf4j.Logger logger=LoggerFactory.getLogger(JenkinsDemoApplication.class);
+
 	public static void main(String[] args) {
-		looger
-		
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
