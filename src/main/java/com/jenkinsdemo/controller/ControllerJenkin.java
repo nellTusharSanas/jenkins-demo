eaaf96fe52a92ed49d270ee0043baf6cd1825577
tushar <tushar.sanas@nellinfotech.com>
2021-01-15 08:35:39 +0530

    write controller

--- src/main/java/com/jenkinsdemo/controller/ControllerJenkin.java
+++ src/main/java/com/jenkinsdemo/controller/ControllerJenkin.java
@@ -1,8 +1,22 @@
 package com.jenkinsdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerJenkin {
+	
+	
+	@GetMapping("/jenkinsDemo")
+	public String jenkinDemo()
+	{
+		try
+		{
+			return "test success";
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+	}
 
 }
